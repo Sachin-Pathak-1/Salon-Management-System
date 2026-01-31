ed3247efcf972bca5c1e490aa5a8b04a68e652da
surajsharma2603 <surajkumarsharma2603@gmail.com>
2026-01-31 17:58:09 +0530

    Converted to Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        text: "var(--text)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        gray100: "var(--gray-100)",
+        borderLight: "var(--border-light)",
+      }
+    },
+  },
+  plugins: [],
+};
